40b5b0467f8f7e7b4ea29db272b9b01299836796
Alin Chiosa <alin.chiosa@gmail.com>
2019-05-16 19:54:45 +0200

    log in + sign up

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+import "./Login.css";
 
 class Login extends React.Component {
     constructor(){
@@ -10,7 +15,33 @@ class Login extends React.Component {
 
     render(){
         return(
-            <div>Login</div>
+            <div className="loginContainer">
+                <h3>Log in</h3>
+                <br/>
+                <Form>
+                    <Form.Group controlId="formBasicEmail" className="formField">
+                        <div className="formLabel">
+                            <Form.Label>Email address:</Form.Label>
+                        </div>
+                        <div className="formText">
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </div>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword" className="formField">
+                        <div className="formLabel">
+                            <Form.Label>Password:</Form.Label>
+                        </div>
+                        <div className="formText">
+                            <Form.Control type="password" placeholder="Password" />
+                        </div>
+                    </Form.Group>
+
+                    <Button variant="secondary" type="submit">
+                        Log in
+                    </Button>
+                </Form>
+            </div>
         )
     }
 }
--- frontend/src/components/signup/Signup.js
+++ frontend/src/components/signup/Signup.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import "./Signup.css";
+
 
 class Signup extends React.Component {
     constructor(){
@@ -10,7 +14,69 @@ class Signup extends React.Component {
 
     render(){
         return(
-            <div>Signup</div>
+            <div className="signupContainer">
+                <h3>Sign up</h3>
+                <br/>
+                <Form>
+                    <Form.Group controlId="formBasicFirstName" className="formField">
+                    <div className="formLabel">
+                        <Form.Label>First name:</Form.Label>
+                    </div>
+                    <div className="formText">
+                        <Form.Control type="text" placeholder="Enter first name" />
+                    </div>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicLastName" className="formField">
+                        <div className="formLabel">
+                            <Form.Label>Last name:</Form.Label>
+                        </div>
+                        <div className="formText">
+                            <Form.Control type="text" placeholder="Enter last name" />
+                        </div>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicEmail" className="formField">
+                        <div className="formLabel">
+                            <Form.Label>Email address:</Form.Label>
+                        </div>
+                        <div className="formText">
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </div>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicDob" className="formField">
+                        <div className="formLabel">
+                            <Form.Label>Date of birt:</Form.Label>
+                        </div>
+                        <div className="formText">
+                            <Form.Control type="dob" placeholder="Enter date of birth" />
+                        </div>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword" className="formField">
+                        <div className="formLabel">
+                            <Form.Label>Password:</Form.Label>
+                        </div>
+                        <div className="formText">
+                            <Form.Control type="password" placeholder="Password" />
+                        </div>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicRepeatPassword" className="formField">
+                        <div className="formLabel">
+                            <Form.Label>Repeat password:</Form.Label>
+                        </div>
+                        <div className="formText">
+                            <Form.Control type="password" placeholder="Password" />
+                        </div>
+                    </Form.Group>
+                    
+                    <Button variant="secondary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
         )
     }
 }
